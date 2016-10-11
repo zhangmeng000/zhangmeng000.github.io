261ab3352fb24e51378c48b451af6a0bb33f3f86
zhangmeng000 <1550685677@qq.com>
2016-10-11 19:01:39 +0800

    d

--- js/music.js
+++ js/music.js
@@ -16,33 +16,33 @@ $(document).ready(function(){
 
 	})
 ;
-	var curIndex=0;
-	var pageNum=$('.page').length-1;
-	console.log(pageNum)//获取当前页数-1
-	touch.on('.main','swipeup',function(e){
-		console.log(curIndex)
-		if(curIndex<pageNum){
-			$('.page').eq(curIndex).addClass('pageUp');//索引值
-			$('.page').eq(curIndex).children('div').addClass('hide');
-			curIndex=curIndex+1;
-			if(curIndex==pageNum){
-				$('.arrow').hide();
-			}
-			$('.page').eq(curIndex).removeClass('pageDown');
-			$('.page').eq(curIndex).children('div').removeClass('hide');
-		}else{return}
-		})
-	touch.on('.main','swipedown',function(e){
-		console.log(curIndex)
-		if(curIndex>0){
-			$('.arrow').show();
-			$('.page').eq(curIndex).addClass('pageDown');
-			$('.page').eq(curIndex).children('div').addClass('hide');//隐藏在出来会有动画效果
-			curIndex=curIndex-1;
-
-			$('.page').eq(curIndex).removeClass('pageUp');
-			$('.page').eq(curIndex).children('div').removeClass('hide');
-		}else{return false}
-
-	})
+	// var curIndex=0;
+	// var pageNum=$('.page').length-1;
+	// console.log(pageNum)//获取当前页数-1
+	// touch.on('.main','swipeup',function(e){
+	// 	console.log(curIndex)
+	// 	if(curIndex<pageNum){
+	// 		$('.page').eq(curIndex).addClass('pageUp');//索引值
+	// 		$('.page').eq(curIndex).children('div').addClass('hide');
+	// 		curIndex=curIndex+1;
+	// 		if(curIndex==pageNum){
+	// 			$('.arrow').hide();
+	// 		}
+	// 		$('.page').eq(curIndex).removeClass('pageDown');
+	// 		$('.page').eq(curIndex).children('div').removeClass('hide');
+	// 	}else{return}
+	// 	})
+	// touch.on('.main','swipedown',function(e){
+	// 	console.log(curIndex)
+	// 	if(curIndex>0){
+	// 		$('.arrow').show();
+	// 		$('.page').eq(curIndex).addClass('pageDown');
+	// 		$('.page').eq(curIndex).children('div').addClass('hide');//隐藏在出来会有动画效果
+	// 		curIndex=curIndex-1;
+  //
+	// 		$('.page').eq(curIndex).removeClass('pageUp');
+	// 		$('.page').eq(curIndex).children('div').removeClass('hide');
+	// 	}else{return false}
+  //
+	//  })
 	})
